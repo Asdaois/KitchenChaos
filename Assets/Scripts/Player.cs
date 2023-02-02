bc57ad763694df71a9d5060fea5b1edb6f28a55c
Jose Guevara <94gjose@gmail.com>
2023-02-01 20:34:10 -0400

    feat: create basic player controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+  [SerializeField, Min(0)]
+  private float moveSpeed = 7f;
+  private void Update() {
+    var inputVector = new Vector2();
+
+    if (Input.GetKey(KeyCode.W)) {
+      inputVector.y += 1;
+    }
+
+    if (Input.GetKey(KeyCode.S)) {
+      inputVector.y -= 1;
+    }
+
+    if (Input.GetKey(KeyCode.A)) {
+      inputVector.x -= 1;
+    }
+
+    if (Input.GetKey(KeyCode.D)) {
+      inputVector.x += 1;
+    }
+
+    var normalizedInputVector = inputVector.normalized;
+    var movementDirection = new Vector3(normalizedInputVector.x, 0, normalizedInputVector.y);
+    transform.position += movementDirection * (Time.deltaTime * moveSpeed);
+
+    Debug.Log(normalizedInputVector);
+  }
+}
