deff72a56fa82b073ed5e5821e936e0fdfc91650
Jose Guevara <94gjose@gmail.com>
2023-02-07 20:36:45 -0400

    feat: add cutting recipes

--- Assets/Scripts/CuttingCounter.cs
+++ Assets/Scripts/CuttingCounter.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 
 public class CuttingCounter : BaseCounter {
-  [SerializeField] private KitchenObjectSO kitchenObjectSO;
+  [SerializeField] private CuttingRecipeSO[] cuttinRecipes;
 
   public override void Interact(Player aPlayer) {
-    if (!HasKitchenObject() && aPlayer.HasKitchenObject()) {
+    if (!HasKitchenObject()
+      && aPlayer.HasKitchenObject()
+      && HasRecipeWithKitchenObject(aPlayer.GetKitchenObject().KitchenObjectSO)) {
       aPlayer.GetKitchenObject().SetKitchenObjepctParent(this);
       return;
     }
@@ -15,12 +17,34 @@ public class CuttingCounter : BaseCounter {
   }
 
   public override void InteractAlternative(Player aPlayer) {
-    if (!HasKitchenObject()) {
+    if (!HasKitchenObject()
+        || !HasRecipeWithKitchenObject(GetKitchenObject().KitchenObjectSO)) {
       return;
     }
 
-    Debug.Log(HasKitchenObject());
+    var outputKitchenObjectSO = GetOutputFromInput(GetKitchenObject().KitchenObjectSO);
+
     GetKitchenObject().DestroySelf();
-    KitchenObject.Spawn(kitchenObjectSO, this);
+    KitchenObject.Spawn(outputKitchenObjectSO, this);
+  }
+
+  private KitchenObjectSO GetOutputFromInput(KitchenObjectSO aKitchenObjectSO) {
+    foreach (var cuttingRecipe in cuttinRecipes) {
+      if (cuttingRecipe.Input == aKitchenObjectSO) {
+        return cuttingRecipe.Output;
+      }
+    }
+
+    return null;
+  }
+
+  private bool HasRecipeWithKitchenObject(KitchenObjectSO aKitchenObjectSO) {
+    foreach (var cuttingRecipe in cuttinRecipes) {
+      if (cuttingRecipe.Input == aKitchenObjectSO) {
+        return true;
+      }
+    }
+
+    return false;
   }
 }
---
+++ Assets/Scripts/CuttingRecipeSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/Recipe")]
+public class CuttingRecipeSO : ScriptableObject {
+  [field: SerializeField] public KitchenObjectSO Input { get; private set; }
+
+  [field: SerializeField] public KitchenObjectSO Output { get; private set; }
+}
